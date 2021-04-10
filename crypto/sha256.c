82fb81437bd931649a721335569c1d00048e7e72
yassineharmouch <65100871+yassineharmouch@users.noreply.github.com>
2021-04-11 00:10:12 +0200

    commit

--- crypto/sha256.c
+++ crypto/sha256.c
@@ -5,7 +5,7 @@
  * @s: Sequence of bytes to be hashed
  * @len: Bytes to hash
  * @digest: array to store resulting hash in
- 
+ *
  * Return: pointer to digest, NULL on error
  */
 uint8_t* sha256(int8_t const* s, size_t len,
