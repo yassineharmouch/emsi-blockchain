bf500c28c767922a616f557081f6c8562567f2ed
yassineharmouch <65100871+yassineharmouch@users.noreply.github.com>
2021-04-18 00:53:21 +0200

    Update ec_verify.c

--- crypto/ec_verify.c
+++ crypto/ec_verify.c
@@ -1,28 +1,18 @@
 #include "hblk_crypto.h"
 
 /**
- * ec_verify - verify the signature of a given set of bytes using public key
- * @key: pointer to EC_KEY struct containing public key
- * @msg: pointer to characters to verify signature of
- * @msglen: len of msg
- * @sig: pointer to signature to check
- *
- * Return: 1 if signature valid, 0 if not
+ * ec_verify - verifies a message with public key
+ * @key: pointer to struct containing key pair
+ * @msg: the message to sign
+ * @msglen: length of message
+ * @sig: address to store signature
+ * Return: 1 if verified else 0
  */
 int ec_verify(EC_KEY const *key, uint8_t const *msg, size_t msglen,
-		sig_t const *sig)
+	sig_t const *sig)
 {
-	unsigned char dgst[SHA256_DIGEST_LENGTH];
-
 	if (!key || !msg || !sig)
 		return (0);
-
-	if (!EC_KEY_check_key(key))
-		return (0);
-	if (!SHA256(msg, msglen, dgst))
-		return (0);
-	if (ECDSA_verify(0, dgst, SHA256_DIGEST_LENGTH, sig->sig, sig->len,
-				(EC_KEY *)key) != 1)
-		return (0);
-	return (1);
+	return (ECDSA_verify(0, msg, msglen, sig->sig, sig->len,
+		(EC_KEY *)key) == 1);
 }
